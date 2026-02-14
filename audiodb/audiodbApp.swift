9faf2371bf0feb3aeb873994f6be3ad0ed8e29fc
Steve Ke <goldenskgaming@gmail.com>
2026-02-14 15:27:28 -0800

    Initial Commit

---
+++ audiodb/audiodbApp.swift
@@ -0,0 +1,17 @@
+//
+//  audiodbApp.swift
+//  audiodb
+//
+//  Created by Changhong Ke on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct audiodbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ audiodbTests/audiodbTests.swift
@@ -0,0 +1,17 @@
+//
+//  audiodbTests.swift
+//  audiodbTests
+//
+//  Created by Changhong Ke on 2/14/26.
+//
+
+import Testing
+@testable import audiodb
+
+struct audiodbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
